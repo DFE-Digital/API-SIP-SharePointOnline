6731d5c8d7d3928c510058e8480ef1fe8e49b74b
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-04-19 14:55:15 +0100

    Removed unused variables and passed missing ones to module

--- terraform/web-app-service.tf
+++ terraform/web-app-service.tf
@@ -7,6 +7,7 @@ module "azure_web_app_services_hosting" {
   tags           = local.tags
 
   launch_in_vnet                 = local.launch_in_vnet
+  virtual_network_address_space  = local.virtual_network_address_space
   service_plan_sku               = local.service_plan_sku
   service_plan_os                = local.service_plan_os
   service_stack                  = local.service_stack
@@ -24,12 +25,17 @@ module "azure_web_app_services_hosting" {
   monitor_slack_webhook_receiver = local.monitor_slack_webhook_receiver
   monitor_slack_channel          = local.monitor_slack_channel
 
-  enable_cdn_frontdoor                         = local.enable_cdn_frontdoor
-  restrict_web_app_service_to_cdn_inbound_only = local.restrict_web_app_service_to_cdn_inbound_only
-  cdn_frontdoor_sku                            = local.cdn_frontdoor_sku
-  cdn_frontdoor_health_probe_path              = local.cdn_frontdoor_health_probe_path
-  cdn_frontdoor_host_add_response_headers      = local.cdn_frontdoor_host_add_response_headers
-  cdn_frontdoor_remove_response_headers        = local.cdn_frontdoor_remove_response_headers
-  cdn_frontdoor_enable_rate_limiting           = local.cdn_frontdoor_enable_rate_limiting
-  cdn_frontdoor_origin_fqdn_override           = local.cdn_frontdoor_origin_fqdn_override
+  enable_cdn_frontdoor                            = local.enable_cdn_frontdoor
+  restrict_web_app_service_to_cdn_inbound_only    = local.restrict_web_app_service_to_cdn_inbound_only
+  cdn_frontdoor_sku                               = local.cdn_frontdoor_sku
+  cdn_frontdoor_health_probe_path                 = local.cdn_frontdoor_health_probe_path
+  cdn_frontdoor_health_probe_interval             = local.cdn_frontdoor_health_probe_interval
+  cdn_frontdoor_response_timeout                  = local.cdn_frontdoor_response_timeout
+  cdn_frontdoor_host_redirects                    = local.cdn_frontdoor_host_redirects
+  cdn_frontdoor_host_add_response_headers         = local.cdn_frontdoor_host_add_response_headers
+  cdn_frontdoor_remove_response_headers           = local.cdn_frontdoor_remove_response_headers
+  cdn_frontdoor_enable_rate_limiting              = local.cdn_frontdoor_enable_rate_limiting
+  cdn_frontdoor_rate_limiting_duration_in_minutes = local.cdn_frontdoor_rate_limiting_duration_in_minutes
+  cdn_frontdoor_rate_limiting_threshold           = local.cdn_frontdoor_rate_limiting_threshold
+  cdn_frontdoor_origin_fqdn_override              = local.cdn_frontdoor_origin_fqdn_override
 }
