32c2c0b64c8a5e25eea6856dae4a9d8fbd135c95
Dan Good <dangood84@me.com>
2023-07-14 09:19:02 +0100

    Updated cypress.config.js to work with Cypress.env('url') env var

--- CypressTests/cypress.config.js
+++ CypressTests/cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+require("dotenv").config(); // Load the .env file
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  env: {
+    url: process.env.url,
   },
 });
