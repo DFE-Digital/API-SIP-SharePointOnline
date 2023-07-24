aa3204c871b43f201a62a5313fb44c86e21a3267
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-07-24 15:37:28 +0100

    Updated to version 0.2.1

--- terraform/web-app-service.tf
+++ terraform/web-app-service.tf
@@ -1,5 +1,5 @@
 module "azure_web_app_services_hosting" {
-  source = "github.com/DFE-Digital/terraform-azurerm-web-app-services-hosting?ref=v0.2.0"
+  source = "github.com/DFE-Digital/terraform-azurerm-web-app-services-hosting?ref=v0.2.1"
 
   environment    = local.environment
   project_name   = local.project_name
