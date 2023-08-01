924aec9466198f7ba82a66e476fd0b344d029ec9
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-08-01 16:32:02 +0100

    Add OWASP ZAP tests and github action

---
+++ CypressTests/cypress/plugins/generateZapReport.js
@@ -0,0 +1,49 @@
+const ZapClient = require('zaproxy')
+const fs = require('fs')
+
+module.exports = {
+  generateZapReport: async () => {
+    const zapOptions = {
+      apiKey: process.env.zapApiKey,
+      proxy: {
+          host: process.env.zapAddress,
+          port: process.env.zapPort
+      }
+    }
+    const zaproxy = new ZapClient(zapOptions)
+    // Wait for passive scanner to finish scanning before generating report
+    let recordsRemaining = 100
+    while (recordsRemaining !== 0) {
+      await zap.pscan.recordsToScan()
+        .then((resp) => {
+          try {
+            recordsRemaining = parseInt(resp.recordsToScan, 10)
+          } catch (err) {
+            if (err instanceof Error) {
+              console.log(`Error converting result: ${err.message}`)
+            } else {
+              console.log('Unknown error during results conversion')
+            }
+            recordsRemaining = 0
+          }
+        })
+        .catch((err) => {
+          console.log(`Error from the ZAP Passive Scan API: ${err}`)
+          recordsRemaining = 0
+        })
+    }
+
+    await zap.reports.generate({
+      title: 'Report',
+      template: 'traditional-html',
+      reportfilename: 'ZAP-Report.html',
+      reportdir: '/zap/wrk'
+    })
+    .then((resp) => {
+      console.log(`${JSON.stringify(resp)}`)
+    })
+    .catch((err) => {
+      console.log(`Error from ZAP Report API: ${err}`)
+    })
+    }
+  }
