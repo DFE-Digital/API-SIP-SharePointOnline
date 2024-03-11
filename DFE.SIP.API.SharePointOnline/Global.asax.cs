4d5f8e66b70bcb98eed284bae667d7de07338d89
mikestock-nimble <mike.stock@nimbleapproach.com>
2024-03-11 11:29:27 +0000

    157331: removed references to sentry

--- DFE.SIP.API.SharePointOnline/Global.asax.cs
+++ DFE.SIP.API.SharePointOnline/Global.asax.cs
@@ -1,6 +1,4 @@
-﻿using Sentry;
-using Sentry.AspNet;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 
 namespace DFE.SIP.API.SharePointOnline
@@ -14,26 +12,6 @@ namespace DFE.SIP.API.SharePointOnline
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
            // RouteConfig.RegisterRoutes(RouteTable.Routes);
            // BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-
-        // Global error catcher
-        protected void Application_Error()
-        {
-            var exception = Server.GetLastError();
-
-            // Capture unhandled exceptions
-            SentrySdk.CaptureException(exception);
-        }
-
-        protected void Application_BeginRequest()
-        {
-            // Start a transaction that encompasses the current request
-            Context.StartSentryTransaction();
-        }
-
-        protected void Application_EndRequest()
-        {
-            Context.FinishSentryTransaction();
         }
     }
 }
