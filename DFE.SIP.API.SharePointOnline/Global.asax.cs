1e8e3c025c00db2618c2214d911ef72d11c53464
Michael Stock <115985723+mikestock-nimble@users.noreply.github.com>
2024-03-11 14:28:55 +0000

    157331: added app insights logging (#109)
    * 157331: added app insights logging
    
    updated app insights dependency to version 22
    removed sentry telemetry
    
    * added secret key for app insights connection string
    
    * 157331: removed references to sentry

--- DFE.SIP.API.SharePointOnline/Global.asax.cs
+++ DFE.SIP.API.SharePointOnline/Global.asax.cs
@@ -1,68 +1,17 @@
-﻿using Sentry;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Routing;
-using Sentry.AspNet;
 
 namespace DFE.SIP.API.SharePointOnline
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        private IDisposable _sentry;
-
         protected void Application_Start()
         {
-            _sentry = SentrySdk.Init(o =>
-            {
-                // We store the DSN inside Web.config; make sure to use your own DSN!
-                o.Dsn = ConfigurationManager.AppSettings["SentryDSN"];
-                o.Environment = ConfigurationManager.AppSettings["Environment"].ToLower();
-                o.SendDefaultPii = true;
-                o.AttachStacktrace = true;
-                o.TracesSampleRate = 1.0;
-#if DEBUG
-                o.Debug = true;
-#endif
-                // Get ASP.NET integration
-                o.AddAspNet();
-            });
-
             // AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
            // RouteConfig.RegisterRoutes(RouteTable.Routes);
            // BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-
-        // Global error catcher
-        protected void Application_Error()
-        {
-            var exception = Server.GetLastError();
-
-            // Capture unhandled exceptions
-            SentrySdk.CaptureException(exception);
-        }
-
-        protected void Application_End()
-        {
-            // Close the Sentry SDK (flushes queued events to Sentry)
-            _sentry?.Dispose();
-        }
-
-        protected void Application_BeginRequest()
-        {
-            // Start a transaction that encompasses the current request
-            Context.StartSentryTransaction();
-        }
-
-        protected void Application_EndRequest()
-        {
-            Context.FinishSentryTransaction();
         }
     }
 }
--- DFE.SIP.API.SharePointOnline/Utilities/TelemetryFactory.cs
+++ DFE.SIP.API.SharePointOnline/Utilities/TelemetryFactory.cs
@@ -13,10 +13,11 @@ namespace DFE.SIP.API.SharePointOnline.Utilities
         {
             if (_telemetryClient == null)
             {
-                var config = new TelemetryConfiguration();
-                config.ConnectionString = $"InstrumentationKey={AppSettings.Get(AppSettings.APPINSIGHTS_KEY)}";
+                // Overrides the ApplicationInsights.config connection string
+                TelemetryConfiguration.Active.ConnectionString = AppSettings.Get(AppSettings.ApplicationInsightsConnectionString);
 
-                _telemetryClient = new TelemetryClient(config);
+                _telemetryClient = new TelemetryClient();
+
                 _telemetryClient.Context.GlobalProperties.Add(InsightMetrics.Environment, AppSettings.Get(AppSettings.Environment));
                 _telemetryClient.Context.GlobalProperties.Add(InsightMetrics.Application, "SIP API SharePoint Online");
                 _telemetryClient.Context.GlobalProperties.Add(InsightMetrics.Function, "Api Request");
